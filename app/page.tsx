9ca9fb2f5545c1912e5e5fd8e047666c73006ae1
starkgit91 <prashant66m@gmail.com>
2025-05-26 16:53:44 +0530

    Install typescript

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import Button from '@/components/ui/button';
+import Card from '@/components/ui/card';
+
 
 export default function Home() {
   return (
